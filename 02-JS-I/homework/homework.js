100f76730a6d793c96cdaa40b88d464f84933bc6
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-03-13 22:17:15 -0500

    Semana1Henry.homework.jsI-jsIV

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,9 +210,23 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  var vocales = ["a","e","i","o","u"]
-  if (letra===vocales){return "Es vocal";} else { return "Dato icorrecto";}
+  //if (letra.length > 1) { return "Dato incorrecto";}
+  //if (letra === "a" || "e" || "i" ||"o" ||"u") {return "Es vocal";} 
+  //return "Dato incorrecto";
+  if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
+
 }
+
+
+
+
+
 
 
 
